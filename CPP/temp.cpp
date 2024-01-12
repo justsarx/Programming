6895b48989f5f4f57e22ede0eac948030ffcd03c
justsarx <sinhasarthak56@gmail.com>
2024-01-12 20:22:05 +0530

    update

--- CPP/temp.cpp
+++ CPP/temp.cpp
@@ -1,37 +1,10 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
- int in, i, mode_class, max_frequency;
- cout << "Enter the number of rows of data :: ";
- cin >> in;
- in++;
- double ci[in][2], f[in], cw;
-
- // Data Input:
- for (i = 1; i < in; i++) {
-   cout << "\nEnter class interval (Lower limit) : ";
-   cin >> ci[i][0];
-   cout << "\nEnter class interval (Upper limit) : ";
-   cin >> ci[i][1];
-   cout << "\nEnter frequency : ";
-   cin >> f[i];
- }
-
- // Find the class with the highest frequency
- mode_class = 1;
- max_frequency = f[1];
- for (i = 2; i < in; i++) {
-   if (f[i] > max_frequency) {
-     mode_class = i;
-     max_frequency = f[i];
-   }
- }
-
- // Calculate the modal class
- cw = ci[1][1] - ci[1][0];
- double mode = ci[mode_class][0] + ((cw / f[mode_class]) * max_frequency);
-
- cout << "Mode: " << mode << endl;
- return 0;
+int main(){
+  string a,b;
+  a="3";
+  b="8";
+  cout<<a+b<<endl;
+  return 0;
 }
