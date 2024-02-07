168e0952bfb5e244f9538f3b6fa3b4b64e099db7
justsarx <sinhasarthak56@gmail.com>
2024-02-07 22:40:48 +0530

    up

--- Java/temp.java
+++ Java/temp.java
@@ -1,7 +1,24 @@
 package Java;
 
+import java.util.Scanner;
+
 public class temp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter Principal amount :: ");
+                double p=sc.nextDouble();
+                System.out.print("\nEnter the Rate :: ");
+                double r =sc.nextDouble();
+                System.out.print("\nEnter the time :: ");
+                double t=sc.nextDouble();
+
+                double si=(p*r*t)/100;
+                double amount = p * Math.pow(1 + r / 100, t);
+                double ci = amount - p;
+                System.out.print("\nThe Simple interest is :: "+ si);
+                System.out.printf("\nThe Compound interest is :: %.2f",ci);
+        }
+
+
     }
 }
