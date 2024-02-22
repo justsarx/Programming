767a12c49773f350e05743daceb765fb568f5b9d
justsarx <sinhasarthak56@gmail.com>
2024-02-22 21:52:57 +0530

    update

---
+++ Python/temp.py
@@ -0,0 +1,14 @@
+class Test:
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+        self.c = 3
+        self.d = 4
+
+    def method(self):
+        print(self.a)
+        print(self.b)
+        print(self.c)
+        print(self.d)
+
+Test().method()
