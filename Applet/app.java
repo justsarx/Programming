eedc8c312bc1e5aeb57847c3d6055579566ecd61
justsarx <sinhasarthak56@gmail.com>
2024-04-02 20:53:05 +0530

    up

---
+++ Applet/app.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.applet.*;
+
+public class app extends Applet{
+    public void paint(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(100, 100, 100, 100);
+    }    
+}
