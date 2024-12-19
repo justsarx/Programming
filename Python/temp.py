e89996d98ea2aff3dd35e4e31c720ecd3cf2d5e5
Sarthak <sinhasarthak56@gmail.com>
2024-12-19 20:56:37 +0530

    remove unused files and add VSCode font settings

---
+++ Python/temp.py
@@ -0,0 +1,25 @@
+def fx(x):
+    return x**2 - 3
+arr = [None] * len(range(0,10))
+for i in range(0,10):
+    arr[i] = fx(i)
+
+print(arr)
+for i in arr:
+    if i < 0:
+        a = arr.index(i)
+    if i > 0:
+        b = arr.index(i)
+        break
+
+print(a,b)
+
+if(fx(a)<0 and fx(b)>0):
+    while(round(a,3)!=round(b,3)):
+        t = (a+b)/2
+        if fx(t) > 0 :
+            b = t
+        else :
+            a = t
+
+print("Root of x^2 - 3 is",round(a,3)) 
